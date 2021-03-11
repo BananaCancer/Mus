ead8c9c05d9de662e9e83ec19260813dd69dcd5d
Rayane Lahrache <raane.lahrache.ldf@gmail.com>
2021-03-11 15:32:25 +0100

    Ajout deuxième joueur à equipe et début adaptation tests

--- src/main/java/com/montaury/mus/jeu/Equipe.java
+++ src/main/java/com/montaury/mus/jeu/Equipe.java
@@ -5,25 +5,31 @@ import com.montaury.mus.jeu.joueur.Joueur;
 public class Equipe {
     private String nomEquipe;
     private Joueur joueurUn;
+    private Joueur joueurDeux;
 
     public static Equipe equipeHumain(String nomEquipe, String nomJoueur)
     {
-        return new Equipe(nomEquipe, Joueur.humain(nomJoueur));
+        return new Equipe(nomEquipe, Joueur.humain(nomJoueur), Joueur.ordinateur());
     }
 
     public static Equipe equipeOdinateurs()
     {
-        return new Equipe("Equipe ordinateurs", Joueur.ordinateur());
+        return new Equipe("Equipe ordinateurs", Joueur.ordinateur(), Joueur.ordinateur());
     }
 
-    public Equipe(String nomE, Joueur joueur1)
+    public Equipe(String nomE, Joueur joueur1, Joueur joueur2)
     {
         this.nomEquipe = nomE;
         this.joueurUn = joueur1;
+        this.joueurDeux = joueur2;
     }
 
     public Joueur getJoueurUn() {
         return joueurUn;
+    }
+
+    public Joueur getJoueurDeux() {
+        return joueurDeux;
     }
 
     public String getNomEquipe() {
